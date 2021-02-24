4aecc6876acc5671477d367aab2dfae998ef1b8f
Sam Oberg <samueloberg@hotmail.com>
2021-02-23 17:58:58 -0800

    added restart button and end page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
 var startButton = document.querySelector('#start');
 var question1 = document.querySelector('#question1');
 var questionHeader1 = document.querySelector('#questionheader1');
-var next1 = document.querySelector('#next1')
+var next1 = document.querySelector('#next1');
 var question2 = document.querySelector('#question2');
 var questionHeader2 = document.querySelector('#questionheader2');
-var next2 = document.querySelector('#next2')
+var next2 = document.querySelector('#next2');
 var question3 = document.querySelector('#question3');
 var questionHeader3 = document.querySelector('#questionheader3');
-var next3 = document.querySelector('#next3')
+var next3 = document.querySelector('#next3');
 var question4 = document.querySelector('#question4');
 var questionHeader4 = document.querySelector('#questionheader4');
-var next4 = document.querySelector('#next4')
+var next4 = document.querySelector('#next4');
 var question5 = document.querySelector('#question5');
 var questionHeader5 = document.querySelector('#questionheader5');
-var next5 = document.querySelector('#next5')
+var next5 = document.querySelector('#next5');
 var question6 = document.querySelector('#question6');
 var questionHeader6 = document.querySelector('#questionheader6');
+var finishBtn = document.querySelector('#finish');
+var finishPage = document.querySelector('.finish');
+var restartBtn = document.querySelector('#restart')
 
 // event listeners to switch questions when user clicks next
 startButton.addEventListener('click', function(event) {
@@ -57,4 +60,14 @@ next5.addEventListener('click', function(event) {
     questionHeader5.style.display = 'none';
     questionHeader6.style.display = 'block';
     question6.style.display = 'block';
+});
+
+finishBtn.addEventListener('click', function(event) {
+    questionHeader6.style.display = 'none';
+    question6.style.display = 'none';
+    finishPage.style.display = 'block';
+});
+restartBtn.addEventListener('click', function(event) {
+    finishPage.style.display = 'none';
+    startButton.style.display = 'block';
 });
